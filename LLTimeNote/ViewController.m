3c3214698173847d11796c492116de2124f99147
tinyMo <baicai003@163.com>
2014-11-07 17:02:22 +0800

    第一次提交

---
+++ LLTimeNote/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LLTimeNote
+//
+//  Created by 楼 彬 on 14-11-7.
+//  Copyright (c) 2014年 lb1006. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LLTimeNote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LLTimeNote
+//
+//  Created by 楼 彬 on 14-11-7.
+//  Copyright (c) 2014年 lb1006. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
